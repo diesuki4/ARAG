84d1e0a6cd864d406764c4a3d5e05acf7204d566
Kyungbin Cho <diesuki4@gmail.com>
2023-06-06 21:38:27 +0900

    프로젝트 생성
    Github 연동
    .gitignore 설정
    .gitattributes 설정
    README 작성

---
+++ README.md
@@ -0,0 +1,3 @@
+# ARAG
+
+언리얼 엔진 5로 제작한 정말 멋진 게임(A Really Awesome Game)입니다.
---
+++ Source/ARAG/ARAGGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ARAGGameMode.h"
+#include "ARAGCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AARAGGameMode::AARAGGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
